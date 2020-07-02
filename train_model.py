f8793cab084494fe7c828b0bbe267c2d32e77acd
unknown <syzek.1@gmail.com>
2020-07-02 12:51:12 -0600

    Still working on quantizing model

--- train_model.py
+++ train_model.py
@@ -3,6 +3,7 @@ import pickle
 import cv2
 import numpy as np
 import tensorflow as tf
+import tensorflow_model_optimization as tfmot
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense, Activation
 from tensorflow.keras.optimizers import Adam
@@ -20,25 +21,24 @@ def nvidia_model():
     model = Sequential(name='Nvidia_Model')
 
     # Convolutional layers
-    model.add(Conv2D(24, (5, 5), strides=(2,2), input_shape=(66, 200, 3), activation='elu'))
-    model.add(Conv2D(36, (5, 5), strides=(2, 2), activation='elu'))
-    model.add(Conv2D(48, (5, 5), strides=(2, 2), activation='elu'))
-    model.add(Conv2D(64, (3, 3), activation='elu'))
+    model.add(Conv2D(24, (5, 5), strides=(2,2), input_shape=(66, 200, 3), activation='relu'))
+    model.add(Conv2D(36, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(Conv2D(48, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(Dropout(0.2)) # Not in original model
-    model.add(Conv2D(64, (3, 3), activation='elu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
 
     # Fully connected layers 
     model.add(Flatten())
     model.add(Dropout(0.2)) # Not in original model
-    model.add(Dense(100, activation='elu'))
-    model.add(Dense(50, activation='elu'))
-    model.add(Dense(10, activation='elu'))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(10, activation='relu'))
 
     # Output layer
     model.add(Dense(3, activation='softmax'))
 
-    optimizer = Adam(learning_rate=1e-3)
-    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
 
@@ -94,28 +94,6 @@ def one_hot(labels):
     one_hot_labels = tf.one_hot(labels_to_ints, NUM_CATEGORIES)
     return one_hot_labels.numpy()
 
-
-
-def save_model(model, X_train):
-    images = tf.cast(X_train, tf.float32)
-    image_ds = tf.data.Dataset.from_tensor_slices((images)).batch(1)
-
-    converter = tf.lite.TFLiteConverter.from_keras_model(model)
-    converter.optimizations = [tf.lite.Optimize.DEFAULT]
-
-    def representative_data_gen():
-        for input_value in image_ds.take(100):
-            # Model has only one input so each data point has one element.
-            yield [input_value]
-    
-    converter.representative_dataset = representative_data_gen
-
-    tflite_model = converter.convert()
-
-    with tf.io.gfile.GFile('nvidia_model_quant.tflite', 'wb') as f:
-        f.write(tflite_model)
-
-
 if __name__ == '__main__':
     X_train_orig, y_train_orig, X_test_orig, y_test_orig = load_training_data()
 
@@ -126,7 +104,23 @@ if __name__ == '__main__':
     y_test = one_hot(y_test_orig)
 
     model = nvidia_model()
-    model.fit(X_train, y_train, batch_size=100, epochs=1)
-
-    save_model(model, X_train)
     
+    quantize_model = tfmot.quantization.keras.quantize_model
+
+    # q_aware stands for for quantization aware.
+    q_aware_model = quantize_model(model)
+
+    # `quantize_model` requires a recompile.
+    q_aware_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    q_aware_model.summary()
+
+    q_aware_model.fit(X_train, y_train, batch_size=100, epochs=1)
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(q_aware_model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+    quantized_tflite_model = converter.convert()
+
+    with tf.io.gfile.GFile('nvidia_model_quant.tflite', 'wb') as f:
+        f.write(quantized_tflite_model)
